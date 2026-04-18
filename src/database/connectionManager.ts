0b774fa503b106b7505cf3a3986c5cdc20270b0d
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2026-04-18 08:35:42 -0300

    Add Files

---
+++ src/database/connectionManager.ts
@@ -0,0 +1,131 @@
+import knex, { type Knex } from "knex";
+
+const oracledb = require("oracledb");
+oracledb.initOracleClient();
+
+const config = {
+	production: {},
+	development: {},
+};
+
+const testConnString = `(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=${process.env.ORCL_HOST})(PORT=${process.env.ORCL_PORT}))(CONNECT_DATA=(SID=${process.env.ORCL_SID})))`;
+
+console.log(testConnString);
+console.log({
+	connection: {
+		user: process.env.ORCL_USER,
+		password: process.env.ORCL_PASS,
+		connectString: testConnString,
+	},
+});
+
+config.production = {
+	client: "oracledb",
+	connection: {
+		user: process.env.ORCL_USER,
+		password: process.env.ORCL_PASS,
+		connectString: testConnString,
+	},
+	pool: {
+		min: 0,
+		max: 5,
+	},
+};
+config.development = {
+	client: "oracledb",
+	connection: {
+		user: process.env.ORCL_USER,
+		password: process.env.ORCL_PASS,
+		connectString: testConnString,
+	},
+	pool: {
+		min: 0,
+		max: 5,
+	},
+};
+
+const getConfig = () => {
+	if (process.env.ENVIRONMENT === "production") {
+		return config.production;
+	}
+	return config.development;
+};
+
+// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
+class ConnectionManager {
+	private static instance: Knex;
+	private static reconnecting = false;
+	private static reconnectAttempts = 0;
+	private static maxReconnectAttempts = 5;
+	private static reconnectInterval = 5000; // 5 segundos
+
+	// Singleton para manter a conexão única
+	public static getConnection(): Knex {
+		if (!ConnectionManager.instance) {
+			ConnectionManager.createConnection();
+		}
+		return ConnectionManager.instance;
+	}
+
+	// Cria a conexão inicial ou recriada
+	private static createConnection(): void {
+		ConnectionManager.instance = knex(getConfig());
+
+		// Escutar erros na conexão
+		// ConnectionManager.instance.raw("SELECT 1").catch((err) => {
+		//   console.error("Initial database connection failed:", err)
+		//   ConnectionManager.attemptReconnect()
+		// })
+
+		// Escuta erros para tentar reconectar
+		ConnectionManager.instance.on("query-error", (err) => {
+			console.error("Query error detected:", err);
+			if (
+				err.code === "ECONNRESET" ||
+				err.code === "PROTOCOL_CONNECTION_LOST"
+			) {
+				if (!ConnectionManager.reconnecting) {
+					ConnectionManager.attemptReconnect();
+				}
+			}
+		});
+	}
+
+	// Recriar conexão no caso de erro crítico
+	private static attemptReconnect(): void {
+		if (
+			ConnectionManager.reconnecting ||
+			ConnectionManager.reconnectAttempts >=
+				ConnectionManager.maxReconnectAttempts
+		) {
+			console.error("Max reconnect attempts reached or already reconnecting.");
+			return;
+		}
+
+		ConnectionManager.reconnecting = true;
+		ConnectionManager.reconnectAttempts++;
+
+		setTimeout(() => {
+			console.warn(
+				`Attempting to reconnect... (${ConnectionManager.reconnectAttempts}/${ConnectionManager.maxReconnectAttempts})`,
+			);
+			try {
+				ConnectionManager.createConnection();
+				ConnectionManager.reconnecting = false;
+				ConnectionManager.reconnectAttempts = 0;
+				console.log("Reconnected to the database successfully.");
+			} catch (err) {
+				console.error("Reconnection attempt failed:", err);
+				ConnectionManager.reconnecting = false;
+				if (
+					ConnectionManager.reconnectAttempts <
+					ConnectionManager.maxReconnectAttempts
+				) {
+					ConnectionManager.attemptReconnect();
+				}
+			}
+		}, ConnectionManager.reconnectInterval);
+	}
+}
+
+export default ConnectionManager;
